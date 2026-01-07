2caf411f37fb91b75fa42c3d467795b1b5afc991
none <89397683+861289009@users.noreply.github.com>
2026-01-07 15:50:13 +0800

    Refactor imports for NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS

--- __init__.py
+++ __init__.py
@@ -1,9 +1,1 @@
-from .video_fade_hex_2 import VideoFadeHex2
-
-NODE_CLASS_MAPPINGS = {
-    "VideoFadeHex2": VideoFadeHex2,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "VideoFadeHex2": "Video Fade Hex (2 Clips)",
-}
+from .video_fade_hex_2in import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
